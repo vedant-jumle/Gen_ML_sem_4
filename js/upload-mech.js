97aff927e2eab9cf364cc919ceabe637344da058
Hriday Keswani <hridaykeswani@gmail.com>
2022-04-17 15:10:27 +0530

    Added static front end for sr_gan and dc_gan

--- js/upload-mech.js
+++ js/upload-mech.js
@@ -5,10 +5,12 @@ function readURL(event,mode){
         document.getElementById('img-upload').hidden = false;
         document.getElementById('upload-btn').hidden = true;
         document.getElementsByClassName('upload-section')[1].style.padding = '0px';
+        document.getElementsByClassName('upload-section')[1].style.border.radius = '0px';
     }else{
         document.getElementById('img-upload1').src = URL.createObjectURL(event.target.files[0]);
         document.getElementById('img-upload1').hidden = false;
         document.getElementById('upload-btn1').hidden = true;
         document.getElementsByClassName('upload-section')[0].style.padding = '0px';
+        document.getElementsByClassName('upload-section')[0].style.border.radius = '0px';
     }
 }
