48ef26af0bdc4b847c1e3c953976305ffb730dbe
Hriday Keswani <hridaykeswani@gmail.com>
2022-04-17 11:47:51 +0530

    upload mechanism for style transfer complete

---
+++ js/upload-mech.js
@@ -0,0 +1,14 @@
+function readURL(event,mode){
+    console.log(URL.createObjectURL(event.target.files[0]));
+    if(mode===0){
+        document.getElementById('img-upload').src = URL.createObjectURL(event.target.files[0]);
+        document.getElementById('img-upload').hidden = false;
+        document.getElementById('upload-btn').hidden = true;
+        document.getElementsByClassName('upload-section')[1].style.padding = '0px';
+    }else{
+        document.getElementById('img-upload1').src = URL.createObjectURL(event.target.files[0]);
+        document.getElementById('img-upload1').hidden = false;
+        document.getElementById('upload-btn1').hidden = true;
+        document.getElementsByClassName('upload-section')[0].style.padding = '0px';
+    }
+}
