864546dc2f0f023db2979d6773fb23d7fb3260d7
maxDeCoder <vedjum@gmail.com>
2022-04-17 18:39:24 +0530

    add project_report.docx

--- generator_models.py
+++ generator_models.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 import cv2
-import PIL
+from PIL import Image
 
 # configure the GPU
 physical_devices = tf.config.list_physical_devices("GPU")
@@ -55,8 +55,28 @@ class StyleTransfer:
         if epoch % 100 == 0:
             tf.print(f"Epoch {epoch} | Loss: {loss}")
 
+    def style_image(self, content_path, style_path, epochs=1000, style_weight=1e-6, content_weight=1e-2):
+        content_image = load_image(content_path)
+        style_image = load_image(style_path)
+
+        content_target = self.vgg_model([content_image*255])[0]
+        style_target = self.vgg_model([style_image*255])[1]
+
+        image = tf.image.convert_image_dtype(content_image, tf.float32)
+        image = tf.Variable(image)
+        
+        for i in range(epochs):
+            self.train_step(image, i, content_target, style_target, content_weight, style_weight)
+        
+        # save the image
+        image = tf.image.convert_image_dtype(image, tf.uint8)
+        image = tf.image.encode_jpeg(image)
+        tf.io.write_file("./output/output.jpg", image)
+
 def load_image(path, shape=(224, 224), normalize=True):
-    img = cv2.resize(cv2.imread(path), shape)
+    img = cv2.imread(path)
+    if shape:
+        img = cv2.resize(img, shape)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = np.array(img, dtype=np.float32)
 
@@ -64,5 +84,80 @@ def load_image(path, shape=(224, 224), normalize=True):
         img = img / 255.0
 
     img = np.expand_dims(img, axis=0)
-    
     return img
+
+class DCGAN:
+    def __init__(self, model_gen_path, latent_dim=128):
+        self.model_gen_path = model_gen_path
+        self.model = self.load_model()
+        self.latent_dim = latent_dim
+
+    def load_model(self):
+        model = keras.models.load_model(self.model_gen_path)
+        return model
+
+    def generate(self):
+        random_vector = tf.random.normal(shape=(1, self.latent_dim))
+        generated_image = self.model(random_vector)[0]
+        generated_image = tf.clip_by_value(generated_image, clip_value_min=0.0, clip_value_max=1.0)
+        generated_image = tf.image.convert_image_dtype(generated_image, tf.uint8)
+        generated_image = tf.image.encode_jpeg(generated_image)
+
+        tf.io.write_file("./output/output.jpg", generated_image)
+
+class SRGAN:
+    def __init__(self, model_gen_path, lr_dim=64, hr_dim=256):
+        self.model_gen_path = model_gen_path
+        self.model = self.load_model()
+        self.lr_dim = lr_dim
+        self.hr_dim = hr_dim
+
+    def load_model(self):
+        model = keras.models.load_model(self.model_gen_path)
+        return model
+
+    def pack(self, img):
+        pack_axis = np.argmin(img.shape[:-1])
+        lower_dim = img.shape[pack_axis]
+        dim_ratio = img.shape[0] / img.shape[1]
+        # resize image to lower_dim, lower_dim
+        img = img * 255
+        img = img.astype(np.uint8)
+        img = Image.fromarray(img)
+        img = img.resize((int(lower_dim), int(lower_dim)))  
+        img = np.array(img)
+        img = img.astype(np.float32)
+
+        return img, dim_ratio, pack_axis
+
+    def unpack(self, img, dim_ratio, axis):
+        # resize image to the higher dimension
+        img = img * 255
+        img = img.astype(np.uint8)
+        img = Image.fromarray(img)
+        if axis == 1:
+            img = img.resize((int(img.size[0] * dim_ratio), int(img.size[1])))
+
+        if axis == 0:
+            img = img.resize((int(img.size[0]), int(img.size[1] * dim_ratio)))
+            
+        return np.array(img, dtype=np.float32) / 255.0
+
+    def downscale(self, img, dims):
+        img = tf.image.resize(img, dims)
+        return img
+
+    def upscale_64_256(self, image_path: str):
+        image = load_image(image_path, shape=None)
+        image = self.downscale(image, (self.lr_dim, self.lr_dim))
+        image, dim_ratio, axis = self.pack(image[0])
+        image = np.expand_dims(image, axis=0)
+
+        ai_upscaled = self.model(image).numpy()
+        ai_upscaled = np.clip(ai_upscaled, 0, 1)
+        ai_upscaled = self.unpack(ai_upscaled[0], dim_ratio, axis)
+
+        ai_upscaled = tf.image.convert_image_dtype(ai_upscaled, tf.uint8)
+        ai_upscaled = tf.image.encode_jpeg(ai_upscaled)
+
+        tf.io.write_file("./generated/test.jpg", ai_upscaled)
