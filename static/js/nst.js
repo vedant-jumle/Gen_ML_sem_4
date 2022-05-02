bc4f11ac07bebd276275b935fe5e6385168f8cf2
maxDeCoder <vedantjumle@gmail.com>
2022-05-02 23:43:43 +0530

    small changes

--- static/js/nst.js
+++ static/js/nst.js
@@ -4,15 +4,15 @@ const output = Qs.parse(location.search, {
 
 if(output!=undefined){
     // left wala image = "NST_content.png"
-    document.getElementById('img-upload1').src="static/NST_content.png";
+    document.getElementById('img-upload1').src="/static/NST_content.png";
     // right wala image = "NST_style.png"
-    document.getElementById('img-upload').src="static/NST_style.png";
+    document.getElementById('img-upload').src="/static/NST_style.png";
 }
 
 function loading(){
-    document.getElementById('img-result').src="static/NST_loading.gif";
+    document.getElementById('img-result').src="/static/loading.gif";
 }
 
 function loaded(){
-    document.getElementById('img-result').src="static/NST_output.png";
+    document.getElementById('img-result').src="/static/NST_output.png";
 }
