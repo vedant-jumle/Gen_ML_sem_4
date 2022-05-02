2f2bcec716e574716095d733dda8675146beac7a
maxDeCoder <vedantjumle@gmail.com>
2022-05-02 23:51:02 +0530

    remove redundant dependencies

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 import os
 from generator_models import *
 
